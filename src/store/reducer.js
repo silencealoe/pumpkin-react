84a329de0766cbe8b021ebbb06cf5db1c0c7a1cb
yanghui <yang_hui@nhn-st.com>
2019-11-07 10:39:51 +0800

    reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,6 +6,10 @@ const defaultdata={
 }
 
 //纯函数：传入什么参数，就返回这个参数，不依赖外部任何状态和数据的变化，函数内部没有与外部的交互，必须只依赖传入的参数
+//为什么要创建副本state
+//redux下所有通过reducer更新state的记录，每一个记录都对应内存中的某一个具体state
+//让用户可以追溯到每一次历史操作产生与执行时,当时的具体状态,这也是使用redux管理状态的重要优势之一
+//若不创建副本,redux的所有操作都将指向内存中的同一个state,我们将无从获取每一次操作前后,state的具体状态与改变,若没有副本,redux-devtools列表里所有的state都将被最后一次操作的结果所取代.我们将无法追溯state变更的历史记录.
 
 export default ((state = defaultdata,action)=>{
   // console.log(state,action)
